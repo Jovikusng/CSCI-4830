7e585c42e4742b141635364fb3c9a8e49ebeab2b
Yi Man <yima1736@colorado.edu>
2018-01-31 06:34:18 +0800

    Update phone.js

--- phone.js
+++ phone.js
@@ -1,1 +1,23 @@
+$(document).ready(function() {
+  $("#tab_bar").show();
+  $("#add_contact").hide();
+  $("#contact_list").hide();
+});
 
+$("#dialer").click(function() {
+  $("#content_dialer").show();
+  $("#add_contact").hide();
+  $("#contact_list").hide();
+});
+
+$("#add").click(function() {
+  $("#add_contact").show();
+  $("#contact_list").hide();
+  $("#content_dialer").hide();
+});
+
+$("#list").click(function() {
+  $("#add_contact").hide();
+  $("#contact_list").show();
+  $("#content_dialer").hide();
+});
